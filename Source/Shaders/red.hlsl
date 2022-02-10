c5bc11fce783635b5537c559916be7c6554b69df
Marko Sreckovic <markosrecko97@hotmail.rs>
2022-02-10 18:38:46 +0000

    [App] Red screen

--- Source/Shaders/red.hlsl
+++ Source/Shaders/red.hlsl
@@ -7,21 +7,16 @@ struct VertexInput
 struct VertexOut
 {
 	float4 pos : SV_POSITION;
-	float2 uv : TEXCOORD;
 };
-
-Texture2D src : register(t0);
 
 VertexOut VS(VertexInput IN)
 {
 	VertexOut OUT;
 	OUT.pos = 0.5f * float4(IN.pos, 0.0f, 1.0f);
-	OUT.uv = IN.uv;
 	return OUT;
 }
 
 float4 PS(VertexOut IN) : SV_Target
 {
-	uint3 pixelCoord = uint3(IN.uv * float2(1024, 768), 0);
-	return src.Load(pixelCoord);
+	return float4(1.0f, 0.0f, 0.0f, 1.0f);
 }
--- Source/main.cpp
+++ Source/main.cpp
@@ -5,6 +5,7 @@
 #include "Render/Commands.h"
 #include "Render/Buffer.h"
 #include "Render/Shader.h"
+#include "Render/Device.h"
 #include "Core/Renderer.h"
 #include "System/VSConsoleRedirect.h"
 #include "System/ApplicationConfiguration.h"
@@ -48,18 +49,46 @@ public:
 
 		m_Shader = GFX::CreateShader("Source/Shaders/red.hlsl");
 		m_VB = GFX::CreateVertexBuffer(fcVBData.size() * sizeof(FCVert), sizeof(FCVert), fcVBData.data());
+
+		D3D11_DEPTH_STENCIL_DESC dsDesc{};
+		dsDesc.DepthEnable = false;
+		dsDesc.StencilEnable = false;
+
+		API_CALL(Device::Get()->GetHandle()->CreateDepthStencilState(&dsDesc, m_DepthState.GetAddressOf()));
+
+		D3D11_RASTERIZER_DESC rsDesc{};
+		rsDesc.CullMode = D3D11_CULL_NONE;
+		rsDesc.FillMode = D3D11_FILL_SOLID;
+
+		API_CALL(Device::Get()->GetHandle()->CreateRasterizerState(&rsDesc, m_RasterizerState.GetAddressOf()));
+
+		D3D11_BLEND_DESC bDesc{};
+		bDesc.RenderTarget[0].BlendEnable = false;
+		bDesc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+
+		API_CALL(Device::Get()->GetHandle()->CreateBlendState(&bDesc, m_BlendState.GetAddressOf()));
+		
 	}
 
 	void OnDraw(ID3D11DeviceContext1* context) override
 	{
+		const float blendFactor[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
 		GFX::Cmd::BindShader(context, m_Shader);
 		GFX::Cmd::BindVertexBuffer(context, m_VB);
+		context->OMSetDepthStencilState(m_DepthState.Get(), 0xff);
+		context->OMSetBlendState(m_BlendState.Get(), blendFactor, 0xff);
+		context->RSSetState(m_RasterizerState.Get());
+
 		context->Draw(6, 0);
 	}
 
 private:
 	BufferID m_VB;
 	ShaderID m_Shader;
+	ComPtr<ID3D11DepthStencilState> m_DepthState;
+	ComPtr<ID3D11RasterizerState> m_RasterizerState;
+	ComPtr<ID3D11BlendState> m_BlendState;
 };
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdParams, int showFlags)
